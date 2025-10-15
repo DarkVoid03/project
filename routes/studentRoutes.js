89ec98224adf832e6251410985053858871ed955
Pratik_Mudaliar <92378372+DarkVoid03@users.noreply.github.com>
2025-10-15 06:18:56 +0530

    Update studentRoutes.js

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const studentController = require("../controllers/studentController");
-// const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("../controllers/authMiddleware");
 
 const router = express.Router();
 
 router.get("/", studentController.getAllStudents);
-router.get("/:id", studentController.getStudentById);
-router.post("/", studentController.createStudent);
-router.put("/:id", studentController.updateStudent);
-router.delete("/:id", studentController.deleteStudent);
+router.get("/:isbn", studentController.getStudentById);
+router.post("/",authMiddleware, studentController.createStudent);
+router.put("/:isbn", authMiddleware,studentController.updateStudent);
+router.delete("/:isbn",authMiddleware, studentController.deleteStudent);
 
 module.exports = router;
+
