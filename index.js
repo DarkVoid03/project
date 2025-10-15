31c5501dc338acaee088dc7ce31eb3a9b192e14c
Pratik_Mudaliar <92378372+DarkVoid03@users.noreply.github.com>
2025-10-15 06:23:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const bodyParser = require("body-parser");
 // const authRoutes = require("./routes/authRoutes");
 const bookRoutes = require("./routes/bookRoutes");
 const studentRoutes = require("./routes/studentRoutes");
+const rentalRoutes = require("./routes/rentalRoutes");
+// const {verifyToken} = require("./controllers/verifyToken");
+const { registerUser, loginUser,verifyToken } = require('./controllers/authController');
+const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+  origin:"http://localhost:3000",
+}))
 
 // Connect to MongoDB
 mongoose
@@ -21,9 +29,16 @@ mongoose
   .catch((err) => console.log(err));
 
 // Routes;
-// app.use("/auth", authRoutes);
+// app.use("/login", authRoutes);
 app.use("/books", bookRoutes);
 app.use("/students", studentRoutes);
+app.use("/rentals", rentalRoutes);
+
+// Mount authentication routes
+app.post('/api/verify', verifyToken)
+app.post('/api/register', registerUser);
+app.post('/api/login', loginUser);
+
 
 // Start server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
